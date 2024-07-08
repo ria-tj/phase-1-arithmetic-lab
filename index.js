ae5d5b958710f400cc5264ea0aace387a3980dee
Ria <johnria6@gmail.com>
2024-07-08 13:29:56 +0300

    first commit

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+1,2 +1,10 
+const num1=31;
+const num2=2;
+const multiply=num1 * num2
+
+// Write your code here
+const random =Math.floor(Math.random()*100) + 1;
+
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+const max =(5,4,3,7,10,20)
